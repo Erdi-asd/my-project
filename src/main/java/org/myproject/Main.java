96e2f05b597c8557ede7a72943bcffded50a4427
Erdi-asd <erdigoci9@gmail.com>
2024-08-14 14:58:34 +0200

    first commit

---
+++ src/main/java/org/myproject/Main.java
@@ -0,0 +1,9 @@
+package org.myproject;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // Stampa un messaggio fisso
+        System.out.println("Ciao dal tuo GitHub Actions!");
+    }
+}
